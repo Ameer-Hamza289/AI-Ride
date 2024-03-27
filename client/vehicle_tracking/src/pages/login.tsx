9c2869010e2fd810f1111c3416ac40aa36ac069b
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-27 06:20:22 +0500

    Login form added basic UI

--- client/vehicle_tracking/src/pages/login.tsx
+++ client/vehicle_tracking/src/pages/login.tsx
@@ -1,9 +1,31 @@
-
+import { ChangeEvent, SyntheticEvent, useState } from "react"
 
 const Login = () => {
+  const [username,setUsername]=useState<string>("");
+  const [password,setPassword]=useState<string>("");
+
+
+  const handleSubmit=(e:SyntheticEvent)=>{
+    e.preventDefault();
+    console.log("Submitted");
+  }
+
   return (
     <div>
-      Login
+      <div className="py-5 text-center ">
+      <h2>Login to Continue!</h2>
+
+      <form onSubmit={handleSubmit}>
+        {/* <div> */}
+            <label htmlFor="username"  className="form-label">Username</label>
+            <input type="text" placeholder="Enter the username " className="form-control" value={username} onChange={(e:ChangeEvent<HTMLInputElement>)=>{setUsername(e.target.value)}} />
+            <label htmlFor="Password" className="form-label">Password</label>
+            <input type="password" placeholder="Enter the password " className="form-control" value={password} onChange={(e:ChangeEvent<HTMLInputElement>)=>{setPassword(e.target.value)}} />
+    <button type="submit" >Login</button>
+          {/* </div> */}
+
+      </form>
+      </div>
     </div>
   )
 }
